28d169532fed7991dfae663e2c817137cc184522
Jesus Ramirez <jesus.zuluaga@correounivalle.edu.co>
2020-05-14 21:00:42 -0500

    kkl

--- miniProyectoFADA/src/miniproyectofada/AlgoritmoIngenuo.java
+++ miniProyectoFADA/src/miniproyectofada/AlgoritmoIngenuo.java
@@ -5,10 +5,42 @@
  */
 package miniproyectofada;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Jesús
  */
 public class AlgoritmoIngenuo {
+    int n;
+    int m;
+    int k;
+    ArrayList listaAnimales;
+    ArrayList listaGrandezas;
+    ArrayList<ArrayList> apertura;
+    ArrayList<ArrayList> partes;
     
+    public void AlgoritmoIngenuo(int n, int m, int k, ArrayList listaAnimales,ArrayList listaGrandezas,ArrayList apertura,ArrayList partes){
+        this.n=n;
+        this.m=m;
+        this.k=k;
+        this.listaAnimales=listaAnimales;
+        this.listaGrandezas=listaGrandezas;
+        this.apertura=apertura;
+        this.partes=partes;
+    }
+    
+    public void OrdenarEscenas(){
+        ArrayList listaApariciones;
+        ArrayList escenasOrdenadas;
+        ArrayList animalesOrdenados;
+        ArrayList parteOrdenada;
+        ArrayList parte;
+        
+        //se ordenan los animales en cada una de las escenas de la apertura
+        for(int i=0; i < (this.m-1)*k; i++){
+            ArrayList escena=this.apertura;
+            
+        }
+    }
 }
